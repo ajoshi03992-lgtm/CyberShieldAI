ebc6426bb441c19071e5ad574ea2ea360034cd81
Aditya Joshi <ajoshi03992@gmail.com>
2026-03-19 12:41:31 +0530

    fixed port binding

--- app.py
+++ app.py
@@ -269,4 +269,5 @@ def admin():
 # ================= RUN APP =================
 if __name__ == "__main__":
 
-    app.run(debug=True)
+    aif __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
