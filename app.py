dca88f7972d8c9c239ce10bda108e555ce4a4779
Aditya Joshi <ajoshi03992@gmail.com>
2026-03-19 13:29:21 +0530

    fixed syntax error

--- app.py
+++ app.py
@@ -269,5 +269,7 @@ def admin():
 # ================= RUN APP =================
 if __name__ == "__main__":
 
-    aif __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=10000)
+
+    
